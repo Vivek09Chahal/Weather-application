f3abd82756c9982efe89003d94d7ceb563128237
Vivek <103368320+Vivek09Chahal@users.noreply.github.com>
2023-06-06 10:10:03 +0530

    Add files via upload

---
+++ temptemp1.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+def get_weather_forecast(city):
+    """Gets the current weather forecast for a city.
+
+    Args:
+        city: The name of the city to get the weather forecast for.
+
+    Returns:
+        A dictionary containing the current weather forecast for the city.
+    """
+
+    api_key = ""
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code != 200:
+        raise Exception(f"Error getting weather forecast for {city}: {data['message']}")
+
+    return data
+
+def main():
+    """The main function."""
+
+    city = input("Enter a city name: ")
+    weather_forecast = get_weather_forecast(city)
+
+    print(f"Current weather forecast for {city}:")
+    print(f"* Temperature: {weather_forecast['main']['temp']}°C")
+    print(f"* Description: {weather_forecast['weather'][0]['description']}")
+
+if __name__ == "__main__":
+    main()
